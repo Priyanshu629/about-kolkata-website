e724455cfec9a79767e86ed5aff1956c6ee1b56a
priyanshu kumar mishra <mishrapriyanshu793@gmail.com>
2024-09-14 10:12:13 +0530

    add intro css

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -9,16 +9,16 @@ const Body = () => {
     <main className='text-white '>
 
 
-      <section className='w-[90%]  sm:w-[70%] mx-auto  my-4'>
-        <h1 className='text-2xl sm:text-4xl  text-center font-bold '>Heritage of Kolkata</h1>
+      <section className='w-[90%] intro sm:w-[70%] mx-auto  my-4 rounded-md border-2 border-gray-100  '>
+        <h1 className='text-2xl sm:text-4xl  text-center font-bold mt-4 '>Heritage of Kolkata</h1>
 
-        <p className='text-xl italic text-yellow-500 my-5 p-2 '>
+        <p className='text-xl font-semibold italic  my-5 p-3 '>
           {intro}
         </p>
       </section>
       <ImageSlider />
       <section className='w-[90%]  mx-auto '>
-        <h2 className='text-3xl sm:text-3xl my-4 text-center font-semibold text-green-400 italic'>Some Interesting Facts</h2>
+        <h2 className='text-3xl sm:text-3xl mt-6 text-center font-bold text-green-400 italic'>Some Interesting Facts</h2>
         <div className='flex flex-wrap justify-center'>
           {facts.map((fact, index) => {
             return <Facts key={index} {...fact} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom"
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false)
     const navref = useRef(null)
+    const listmenu = useRef(null)
 
     useEffect(() => {
 
@@ -19,24 +20,43 @@ const Header = () => {
 
     }
     const close = () => {
-        navref.current.style.left = "-90%"
+        navref.current.style.left = "-100%"
         setIsOpen(false)
     }
+
+
     return (
-        <header className="backdrop sticky top-0 h-[100px] text-orange-400 flex items-center justify-between p-4 ">
-            <div className=" ">
-                <img className="w-[80px] h-[80px] rounded-full" src={logo} alt="logo" />
-            </div>
-            <nav ref={navref} className="max-sm:bg-slate-800 text-xl font-bold mx-4 max-sm:w-[90%] max-sm:absolute top-[100px] left-[-90%] max-sm:flex max-sm:flex-col max-sm:items-center
+        <>
+            <header className="backdrop sticky top-0 h-[100px] text-orange-100 flex items-center justify-between p-4 ">
+                <div className=" ">
+                    <img className="w-[80px] h-[80px] rounded-full" src={logo} alt="logo" />
+                </div>
+                <nav ref={navref} className="max-sm:bg-slate-800 text-xl font-bold mx-4 max-sm:w-[100%] max-sm:absolute top-[100px] left-[-100%] max-sm:flex max-sm:flex-col max-sm:items-center
             max-sm:m-0 max-sm:h-[300px] max-sm:justify-center max-sm:opacity-[0.9]">
-                <Link to="" className="mx-2  link">Heritages</Link>
-                <Link to="" className="mx-2 link max-sm:my-3">Contact Us</Link>
-                <Link to="" className="mx-2 link max-sm:my-3">Gallery</Link>
-            </nav>
-            {isOpen ? <span onClick={close} className="sm:hidden text-4xl font-bold">&times;</span> :
-                <span onClick={toggle} className="sm:hidden text-4xl font-bold">&#9776;</span>
-            }
-        </header>
+
+
+                    <Link to="" className="mx-2 max-sm:hidden link rounded-md   text-white" onMouseOver={() => {
+                        listmenu.current.style.display = "flex"
+                    }}>
+                        Explore ⬇️
+                    </Link>
+                    <Link to="" className="link sm:hidden rounded-md px-2" >Places-To-Visit</Link>
+                    <Link to="" className="link max-sm:my-3 sm:hidden rounded-md px-2">Famous-Food</Link>
+                    <Link to="" className="mx-2 link max-sm:my-3 rounded-md px-2">Contact Us</Link>
+                    <Link to="" className="mx-2 link max-sm:my-3 rounded-md px-2">Gallery</Link>
+                </nav>
+
+                {isOpen ? <span onClick={close} className="sm:hidden text-4xl font-bold">&times;</span> :
+                    <span onClick={toggle} className="sm:hidden text-4xl font-bold">&#9776;</span>
+                }
+            </header>
+            <div onMouseLeave={() => {
+                listmenu.current.style.display = "none"
+            }} ref={listmenu} className="absolute rounded-md right-[200px] top-[70px]  w-[150px] h-[200px] bg-slate-500  text-white hidden flex-col justify-center items-center">
+                <a href="" className="text-yellow-100 font-bold link">Places-To-Visit</a>
+                <a href="" className="my-4 text-yellow-100 font-bold link">Famous-Food</a>
+            </div>
+        </>
     )
 }
 
